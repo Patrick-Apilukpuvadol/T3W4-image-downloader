4897eaa9f7744f3a9aaee48718aded3379a8383e
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-09-09 12:16:24 +1000

    feat: fixed mock testing

---
+++ tests/downloader.test.js
@@ -0,0 +1,36 @@
+const {
+    getPokemonPictureUrl
+} = require("../downloader");
+
+describe("Retrieve a Pokemon URL", () => {
+    // id ID is 25, URL should be:
+    // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png
+    let expectedImageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+
+    let expectedJsonData = {
+        sprites: {
+            other: {
+                'official-artwork': {
+                    front_default: expectedImageUrl
+                }
+            }
+        }
+    }
+
+    globalThis.fetch = jest.fn(() => {
+        console.log("Fetch has been repplaced with a Jest mock!");
+        return new Promise((resolve, reject) => {
+            resolve({
+                json: () => {
+                    return Promise.resolve(expectedJsonData);
+                }
+            });
+        })
+    })
+
+    test("If given an ID of 25, url is expected correct URL", async () => {
+        let result = await getPokemonPictureUrl(25);
+
+        expectedImageUrl(result).toEqual(expectedImageUrl);
+    })
+})
